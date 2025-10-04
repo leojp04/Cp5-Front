e3bca87b4157d69d432f881653523f4f56ef5ac5
leojp04 <leojp04@gmail.com>
2025-10-04 19:06:07 -0300

    feat(ui): spinner

---
+++ access-control-cp/src/components/Spinner.tsx
@@ -0,0 +1,7 @@
+export default function Spinner() {
+  return (
+    <div className="min-h-[40vh] grid place-items-center">
+      <div className="animate-pulse text-zinc-400 text-sm">carregando…</div>
+    </div>
+  );
+}
